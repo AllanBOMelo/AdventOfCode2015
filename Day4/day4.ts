0b653f0ebaf58dd577b8dde73dfed5133dd83671
Allan Barbosa <115508413+AllanBOMelo@users.noreply.github.com>
2023-10-13 16:50:39 +0000

    Frustracao

--- Day4/day4.ts
+++ Day4/day4.ts
@@ -6,7 +6,28 @@ let input: String = 'yzbqklnj';
 let input_number: number = 11111111
 var control: Boolean = true
 
-let test : number = 500
+//test.toString(16).split('',2)
+//socorro.join("")
 
-console.log(test.toString(16))
+let cont: number = 0
 
+while (control) {
+    console.log(cont)
+
+    let verify: any = input + input_number.toString()
+
+    let hex: any = ""
+
+    for (let i: number = 0 ; i < verify.length ; i++) {
+        hex += verify[i].toString(16)
+    }
+
+    let teste: any = hex.split("", 5)
+
+    teste == "00000" ? control = false : input_number += 1
+    if (cont == 99999999) control = false
+
+    cont += 1
+}
+
+console.log(input_number)
