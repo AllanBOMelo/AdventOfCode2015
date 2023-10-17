406f000163fc0151b4f8c2cdc2e009a36dc04b8b
Allan Barbosa <115508413+AllanBOMelo@users.noreply.github.com>
2023-10-17 16:51:03 +0000

    md5 hash library

--- Day4/day4.ts
+++ Day4/day4.ts
@@ -1,33 +1,17 @@
 import * as fs from "fs";
+import {Md5} from 'ts-md5';
 
 console.log("--- Init ---");
 
 let input: String = 'yzbqklnj';
-let input_number: number = 11111111
+let input_number: number = 1
 var control: Boolean = true
+//var retorno: string
 
 //test.toString(16).split('',2)
 //socorro.join("")
+let retorno: string = input + input_number.toString()
 
-let cont: number = 0
+while (control)
 
-while (control) {
-    console.log(cont)
-
-    let verify: any = input + input_number.toString()
-
-    let hex: any = ""
-
-    for (let i: number = 0 ; i < verify.length ; i++) {
-        hex += verify[i].toString(16)
-    }
-
-    let teste: any = hex.split("", 5)
-
-    teste == "00000" ? control = false : input_number += 1
-    if (cont == 99999999) control = false
-
-    cont += 1
-}
-
-console.log(input_number)
+console.log(Md5.hashStr(retorno))
--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,4 @@
 npm i ts-node
 npm i @types/node
 npx tsc --init
+npm install ts-md5
