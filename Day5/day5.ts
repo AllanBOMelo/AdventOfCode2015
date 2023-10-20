aabdcf4e355f9ba89b790b3652fd49c6b9a7d023
Allan Barbosa <115508413+AllanBOMelo@users.noreply.github.com>
2023-10-20 16:49:18 +0000

    Trying Again

--- Day5/day5.ts
+++ Day5/day5.ts
@@ -2,11 +2,10 @@ import * as fs from 'fs';
 
 console.log('--- Init ---')  
 
-let rawInput: String = fs.readFileSync('./Day5/input5.txt','utf8');
-let inputArray: String[] = rawInput.split("\n")
-let nice: number = 0
-let instructions: string[] =  ['ab', 'cd', 'pq', 'xy', 'a', 'e', 'i', 'o', 'u']
-
+let rawInput: string = fs.readFileSync('./Day5/input5.txt','utf8');
+let inputArray: string[] = rawInput.split("\n")
+var nice: number = 0
+let vowel: string[] = ['a', 'e', 'i', 'o', 'u']
 
 //It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
 //It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
@@ -14,22 +13,60 @@ let instructions: string[] =  ['ab', 'cd', 'pq', 'xy', 'a', 'e', 'i', 'o', 'u']
 
 //vogais.indexOf(letra.toLowerCase()) !== -1
 
+function isnaughtyFunction (item: string) {
+
+    if (item.includes('ab') || item.includes('cd') || item.includes('pq') || item.includes('xy')) {
+        return true
+    } else { 
+        return false
+    }
+
+}
+
+function howManyVowels (item: string) {
+    let counter: number = 0;
+    let splitted: string[] = item.split("")
+
+    splitted.forEach((element) => {
+        if (vowel.includes(element)) counter += 1
+    });
+
+
+    return counter
+}
+
+function howManyDouble (item:string) {
+    let counter: number = 0;
+    let array: string[] = item.split("")
+
+    array.forEach((element, index) => {
+        if (element == array[index + 1] && index < array.length - 2) counter += 1
+    })
+
+    return counter
+}
+
+////
+
 inputArray.forEach((item) => {
-        let vowels: number = 0;
-        let doubleLetter: number = 0;
-        var isnaughty : boolean = false;
+    let vowels: number = howManyVowels(item);
+    let doubleLetter: number = howManyDouble(item);
+    var isnaughty : boolean = isnaughtyFunction(item);
 
-        for (let i: number = 0 ; i < 9 ; i++) {
-            if (item.includes(instructions[i]) == false) {
-                i < 4 ? isnaughty = true : vowels+= 1
-            }
-        }
+    console.log(`${item} vowe: ${vowels} double: ${doubleLetter} naughty: ${isnaughty}`)
+
+    if (isnaughty == false && vowels > 2  && doubleLetter > 0 ) {
+        nice += 1
         
-        for (let i: number = 0 ; i < item.length ; i++) {
-            if (item[i] == item[i+1] )  doubleLetter += 1
-        }
-
-        if (vowels > 0 && isnaughty == false || doubleLetter > 0 && isnaughty == false) nice += 1
+    } //else {
+        //console.log(`${item} vowe: ${vowels} double: ${doubleLetter}`)
+    //}
 })
+
+
+
+
+/////
+
 
 console.log(nice)
